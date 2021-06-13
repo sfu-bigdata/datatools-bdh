67413331f47c02d130f7d57244475ea371705e73
Steven Bergner <git-steb@users.noreply.github.com>
2021-06-13 15:31:39 -0700

    Update pccf module

--- README.md
+++ README.md
@@ -4,7 +4,41 @@ Utility and helper functions used at SFU's Big Data Hub
 
 The modules in this package mostly provide additional helper functions to work with other comprehensive libraries or environments, such as Pandas or IPython.
 
+## Content
+
 |   |   |
 |---|---|
 | `ipython` | Utilities to work with IPython and some Pandas  |
-| `pccf` | Postal code conversion file (Stats Can) |
+| `pccf` | Working with Canadian postal codes via the Stats Can Postal code conversion file |
+| `data_uri` | Generate Data URIs |
+
+## Installation
+
+It is possible to install this package directly from its public github location:
+
+`pip install git+https://github.com/sfu-bigdata/datatools-bdh/tree/main/datatools_bdh`
+
+Alternatively, you can clone the repo and install locally:
+```
+git clone https://github.com/sfu-bigdata/datatools-bdh/tree/main/datatools_bdh
+cd datatools_bdh
+pip install -e .
+```
+The `-e` option installs the module via symbolic links, so code changes during development propagate more easily.
+
+## Usage
+IPython utilities:
+```
+from datatools_bdh.ipython import *
+
+% markdown This is *single-line* markdown content within a Python cell.
+```
+
+Postal code conversion file:
+```
+import datatools_bdh.pccf as pccf
+pccf.init('...pccf.txt filename...')
+
+# access the PCCF DataFrame:
+pccf.df
+```
--- datatools_bdh/pccf.py
+++ datatools_bdh/pccf.py
@@ -1,19 +1,39 @@
-""" Tools to work with Statistics Canada's Postal code conversion file
+""" Tools to work with Statistics Canada's Postal code conversion file (pccf)
+
+To work with the pccf, first initialize:
+import datatools_bdh.pccf as pccf
+
+`pccf.init('../../files/pccfNat_fccpNat_082020.txt')`
+
+The use the conversion dataframe either via:
+`pccf.df`
+or via
+`pccf.pccf_df()`
 """
 
 from datatools_bdh import _get_resource_path
 import pandas as pd
 
-rldf = None
-pccf_df = None
+_rldf = None
+_pccf_df = None
 
-def init_pccf(filename):
+def pccf_df():
+    return _pccf_df
+
+def __getattr__(name):
+    if name == 'rl':
+        return _rldf
+    if name == 'df':
+        return _pccf_df
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+def init(filename):
     """Load the raw 2020 Postal code conversion file from given `filename`
        Call this with the location of pccfNat_fccpNat_082020.txt
     """
-    global pccf_df, rldf
+    global _pccf_df, _rldf
 
-    rldf = pd.read_csv(_get_resource_path('pccf_2020_record_layout.csv'))
+    _rldf = pd.read_csv(_get_resource_path('pccf_2020_record_layout.csv'))
 
     # load the raw text pccf file
     with open(filename,'r', encoding='latin-1') as fh:
@@ -25,15 +45,15 @@ def init_pccf(filename):
     pcs = pd.Series(pclines)
 
     def gen_pccf():
-        for idx, r in rldf.iterrows():
+        for idx, r in _rldf.iterrows():
             yield r['Field name'], pcs.str[r['Position']-1:r['Position']+r['Size']-1]
             
-    pccf_df = pd.DataFrame(dict(gen_pccf()))
+    _pccf_df = pd.DataFrame(dict(gen_pccf()))
 
     # remove whitespace around string fields
-    pccf_df["Comm_Name"] = pccf_df["Comm_Name"].str.strip()
-    pccf_df["Community"] = pccf_df["Comm_Name"].str.replace("-"," ").str.title()
-    pccf_df["CSDname"] = pccf_df["CSDname"].str.strip()
+    _pccf_df["Comm_Name"] = _pccf_df["Comm_Name"].str.strip()
+    _pccf_df["Community"] = _pccf_df["Comm_Name"].str.replace("-"," ").str.title()
+    _pccf_df["CSDname"] = _pccf_df["CSDname"].str.strip()
     # lat/lon as floating point (N type in record layout)
-    pccf_df["LAT"] = pccf_df["LAT"].astype(float)
-    pccf_df["LONG"] = pccf_df["LONG"].astype(float)
+    _pccf_df["LAT"] = _pccf_df["LAT"].astype(float)
+    _pccf_df["LONG"] = _pccf_df["LONG"].astype(float)
